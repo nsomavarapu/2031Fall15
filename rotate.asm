2167edf331c23a3dbc6ecf6f593919f623d8a869
Nathan Somavarapu <nsomavarapu3@gatech.edu>
2015-11-10 15:09:53 -0500

    Added in progress files.

---
+++ rotate.asm
@@ -0,0 +1,12 @@
+rotateLeft:
+	LOAD FMid
+	OUT RVELCMD
+	LOAD RMid
+	OUT LVELCMD
+
+rotateRight:
+	LOAD RMid
+	OUT RVELCMD
+	LOAD LMid
+	OUT LVELCMD
+	
---
+++ seekNextPoint.asm
@@ -0,0 +1,145 @@
+seekNextPoint:
+	LOAD Zero
+	OUT RESETPOS
+NotAtPoint:
+	LOAD CDX
+	SUB CX
+	STORE AtanX
+	Load CDY
+	SUB CY
+	STORE AtanY
+	CALL Atan2
+	STORE currAngCalc
+	IN THETA
+	SUB currAngCalc
+	JNEG lessThan
+greaterThan:
+	CALL rotateRight
+	IN THETA
+	SUB currAngCalc
+	JPOS greaterThan
+	JUMP Cont
+lessThan:
+	CALL rotateLeft
+	IN THETA
+	SUB currAngCalc
+	JNEG lessThan
+	
+Cont:
+	LOAD FMid
+	OUT RVELCMD
+	OUT LVELCMD
+	IN XPOS
+	SUB CDX
+	J
+	
+	
+	
+	
+
+
+;***************************************************************
+;* Variables
+;***************************************************************
+Temp:  DW 0 ; "Temp" is not a great name, but can be useful
+Temp2: DW 0
+Temp3: DW 0
+currAngCalc: DW 0  ; Current Angle calculated in atan2.
+CDX: DW 0      ; current desired X
+CDY: DW 0      ; current desired Y
+CDT: DW 0      ; current desired angle
+CX:  DW 0      ; sampled X
+CY:  DW 0      ; sampled Y
+CT:  DW 0      ; sampled theta
+
+;***************************************************************
+;* Constants
+;* (though there is nothing stopping you from writing to these)
+;***************************************************************
+NegOne:   DW -1
+Zero:     DW 0
+One:      DW 1
+Two:      DW 2
+Three:    DW 3
+Four:     DW 4
+Five:     DW 5
+Six:      DW 6
+Seven:    DW 7
+Eight:    DW 8
+Nine:     DW 9
+Ten:      DW 10
+
+; Some bit masks.
+; Masks of multiple bits can be constructed by ORing these
+; 1-bit masks together.
+Mask0:    DW &B00000001
+Mask1:    DW &B00000010
+Mask2:    DW &B00000100
+Mask3:    DW &B00001000
+Mask4:    DW &B00010000
+Mask5:    DW &B00100000
+Mask6:    DW &B01000000
+Mask7:    DW &B10000000
+LowByte:  DW &HFF      ; binary 00000000 1111111
+LowNibl:  DW &HF       ; 0000 0000 0000 1111
+
+; some useful movement values
+OneMeter: DW 952       ; ~1m in 1.05mm units
+HalfMeter: DW 476      ; ~0.5m in 1.05mm units
+OneFoot:  DW 290       ; ~1ft in 1.05mm robot units
+TwoFeet:  DW 581       ; ~2ft in 1.05mm units
+Deg90:    DW 90        ; 90 degrees in odometer units
+Deg180:   DW 180       ; 180
+Deg270:   DW 270       ; 270
+Deg360:   DW 360       ; can never actually happen; for math only
+FSlow:    DW 100       ; 100 is about the lowest velocity value that will move
+RSlow:    DW -100
+FMid:     DW 350       ; 350 is a medium speed
+RMid:     DW -350
+FFast:    DW 500       ; 500 is almost max speed (511 is max)
+RFast:    DW -500
+
+MinBatt:  DW 130       ; 13.0V - minimum safe battery voltage
+I2CWCmd:  DW &H1190    ; write one i2c byte, read one byte, addr 0x90
+I2CRCmd:  DW &H0190    ; write nothing, read one byte, addr 0x90
+
+;***************************************************************
+;* IO address space map
+;***************************************************************
+SWITCHES: EQU &H00  ; slide switches
+LEDS:     EQU &H01  ; red LEDs
+TIMER:    EQU &H02  ; timer, usually running at 10 Hz
+XIO:      EQU &H03  ; pushbuttons and some misc. inputs
+SSEG1:    EQU &H04  ; seven-segment display (4-digits only)
+SSEG2:    EQU &H05  ; seven-segment display (4-digits only)
+LCD:      EQU &H06  ; primitive 4-digit LCD display
+XLEDS:    EQU &H07  ; Green LEDs (and Red LED16+17)
+BEEP:     EQU &H0A  ; Control the beep
+CTIMER:   EQU &H0C  ; Configurable timer for interrupts
+LPOS:     EQU &H80  ; left wheel encoder position (read only)
+LVEL:     EQU &H82  ; current left wheel velocity (read only)
+LVELCMD:  EQU &H83  ; left wheel velocity command (write only)
+RPOS:     EQU &H88  ; same values for right wheel...
+RVEL:     EQU &H8A  ; ...
+RVELCMD:  EQU &H8B  ; ...
+I2C_CMD:  EQU &H90  ; I2C module's CMD register,
+I2C_DATA: EQU &H91  ; ... DATA register,
+I2C_RDY:  EQU &H92  ; ... and BUSY register
+UART_DAT: EQU &H98  ; UART data
+UART_RDY: EQU &H98  ; UART status
+SONAR:    EQU &HA0  ; base address for more than 16 registers....
+DIST0:    EQU &HA8  ; the eight sonar distance readings
+DIST1:    EQU &HA9  ; ...
+DIST2:    EQU &HAA  ; ...
+DIST3:    EQU &HAB  ; ...
+DIST4:    EQU &HAC  ; ...
+DIST5:    EQU &HAD  ; ...
+DIST6:    EQU &HAE  ; ...
+DIST7:    EQU &HAF  ; ...
+SONALARM: EQU &HB0  ; Write alarm distance; read alarm register
+SONARINT: EQU &HB1  ; Write mask for sonar interrupts
+SONAREN:  EQU &HB2  ; register to control which sonars are enabled
+XPOS:     EQU &HC0  ; Current X-position (read only)
+YPOS:     EQU &HC1  ; Y-position
+THETA:    EQU &HC2  ; Current rotational position of robot (0-359)
+RESETPOS: EQU &HC3  ; write anything here to reset odometry to 0
